5c12c848243742b516f394773a8b048a58ab9e59
Jay P <103327654+jpgf07@users.noreply.github.com>
2022-10-07 14:57:27 -0500

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo "This is a TEST ONLY.... THANKS"
+echo "Installing Additional R Libraries"
+R --vanilla
+#ADD LIBRARIES PACKAGES HERE#
+install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+#CLOSING R PACKAGE MANAGER
+q()
+echo "END"
