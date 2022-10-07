c1c614080b14c634041b2f5f893bb5ae97e602b4
Jay P <103327654+jpgf07@users.noreply.github.com>
2022-10-07 15:27:08 -0500

    Update Rlibraries.sh

--- Rlibraries.sh
+++ Rlibraries.sh
@@ -3,6 +3,5 @@ echo "Installing Additional R Libraries"
 #ADD LIBRARIES PACKAGES HERE#
 Rscript -e 'install.packages("glue", repos="http://cran.us.r-project.org")'
 Rscript -e 'install.packages("drat", repos="http://cran.us.r-project.org")'
-#CLOSING R PACKAGE MANAGER
-q()
+#ADD LIBRARIES ABOVE HERE
 echo "End"
