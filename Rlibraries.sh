12fffad43bbdf33c0bbab51ce9fe91d247a9d60f
Jay P <103327654+jpgf07@users.noreply.github.com>
2022-10-11 15:08:24 -0500

    Update Rlibraries.sh

--- Rlibraries.sh
+++ Rlibraries.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 echo "Installing Additional R Libraries"
 #ADD LIBRARIES PACKAGES HERE#
-Rscript -e 'install.packages("glue", repos="http://cran.us.r-project.org")'
-Rscript -e 'install.packages("drat", repos="http://cran.us.r-project.org")'
+Rscript -e 'renv::activate()'
+Rscript -e 'renv::install("bioc::Biobase")'
 #ADD LIBRARIES ABOVE HERE
 echo "End"
+
+
+
+
+
+
+
+
+
+###  Original
+###  #!/bin/bash
+###  echo "Installing Additional R Libraries"
+###  #ADD LIBRARIES PACKAGES HERE#
+###  Rscript -e 'install.packages("glue", repos="http://cran.us.r-project.org")'
+###  Rscript -e 'install.packages("drat", repos="http://cran.us.r-project.org")'
+###  #ADD LIBRARIES ABOVE HERE
+###  echo "End"
